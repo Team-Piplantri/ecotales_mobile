2ecf2b4d3be015fdd43a08a88f250fb06a8e8404
Shubh Pathak <shubhpathak07@gmail.com>
2021-06-23 00:07:45 +0530

    Fixed Context

--- UserContext.js
+++ UserContext.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 const UserContext = React.createContext({
-    isLogin: true,
-    setLogin: () => {}
+    isLoginContext: false,
+    setLoginContext: () => {}
 });
 
 export default UserContext
